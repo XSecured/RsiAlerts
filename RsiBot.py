251895867eb985a6b823e558aead8e92c84a1f66
XSecured <32965591+XSecured@users.noreply.github.com>
2025-05-04 17:24:51 +0300

    Update RsiBot.py

--- RsiBot.py
+++ RsiBot.py
@@ -18,7 +18,7 @@ import glob
 # === CONFIG & CONSTANTS ===
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[logging.StreamHandler()]
 )
